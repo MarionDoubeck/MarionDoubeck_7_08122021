56596e42675ffaefa25153436e0100211d464679
Marion Doubeck <mariondoubeck@yahoo.fr>
2021-12-15 16:35:34 +0100

    const dans for

--- functions.js
+++ functions.js
@@ -2,11 +2,11 @@ function init_list_of_tags(list_of_recipies){
   console.log('dans init_list_of_tags');
   let list_of_tags=[];
   let tag=new Tag;
-  for (const recipe of list_of_recipies){
-    for(let i=0;i<recipe.ingredients.length;i++){
+  for(const recipe of list_of_recipies){
+    for(const  i=0;i<recipe.ingredients.length;i++){
       tag=new Tag(recipe['ingredients'][i]['ingredient'].toLowerCase(),'ingredients');
       let alreadyIn=0;
-        for (let k=0;k<list_of_tags.length;k++){
+        for(const  k=0;k<list_of_tags.length;k++){
           if(list_of_tags[k].name==tag.name && list_of_tags[k].type=="ingredients"){
             alreadyIn++;
           }
@@ -17,7 +17,7 @@ function init_list_of_tags(list_of_recipies){
     }
     tag=new Tag(recipe['appliance'].toLowerCase(),'appliance');
     let alreadyIn=0;
-    for(let k=0;k<list_of_tags.length;k++){
+    for(const  k=0;k<list_of_tags.length;k++){
       if(list_of_tags[k].name==tag.name && list_of_tags[k].type=="appliance"){
         alreadyIn++;
       }
@@ -25,7 +25,7 @@ function init_list_of_tags(list_of_recipies){
     if (alreadyIn==0){
       list_of_tags.push(tag);
     }
-    for (let i=0;i<recipe['ustensils'].length;i++){
+    for(const  i=0;i<recipe['ustensils'].length;i++){
       tag=new Tag(recipe['ustensils'][i].toLowerCase(),'ustensils');
       if(list_of_tags.find(element=>(element.name==tag.name && element.type=='ustensils'))==undefined){
         list_of_tags.push(tag);
@@ -48,8 +48,8 @@ function filter_recipies(recipies){
   let newList=[];
   let item="";
   let addIt=0;
-  for (let recipe of recipies){
-    for(let tag of list_of_displayed_tags){
+  for (const  recipe of recipies){
+    for(const  tag of list_of_displayed_tags){
       tag.name=tag.name.toLowerCase();
       if(tag.type=='appliance'){
         item=recipe[tag.type].toLowerCase();
@@ -57,7 +57,7 @@ function filter_recipies(recipies){
           addIt++;
         }
       }else{
-        for(let temp of recipe[tag.type]){
+        for(const  temp of recipe[tag.type]){
           if(tag.type=="ingredients"){
             item=temp['ingredient'].toLowerCase();
             if (item==tag.name){
@@ -85,7 +85,7 @@ function filter_recipies(recipies){
   }
   let newList2=[];
   number_of_recipies=recipies.length;
-  for (let i=0;i<number_of_recipies;i++){
+  for (const  i=0;i<number_of_recipies;i++){
     const title=recipies[i].name;
     const ingredients=recipies[i].ingredients;
     const description=recipies[i].description;
@@ -94,8 +94,8 @@ function filter_recipies(recipies){
     }
   }
   let sorted_recipies_list=[];
-  for(recipe of newList){
-    for(recipe2 of newList2){
+  for(const recipe of newList){
+    for(const recipe2 of newList2){
       if(recipe==recipe2){
         sorted_recipies_list.push(recipe);
       }
@@ -111,11 +111,11 @@ function filter_advanced_search_fields(type,sorted_recipies_list,recipies){
   switch (type){
     case 'ingredients':
       list_of_the_field=[];
-      for(recipe of sorted_recipies_list){
-        for(nb_of_ingredients in recipe.ingredients){
+      for(const recipe of sorted_recipies_list){
+        for(const nb_of_ingredients in recipe.ingredients){
           const myIngredient=recipe.ingredients[nb_of_ingredients].ingredient.toLowerCase();
           let alreadyIn=0;
-          for(element of list_of_the_field){
+          for(const element of list_of_the_field){
             if(element.name==myIngredient){
               alreadyIn++;
             }
@@ -130,10 +130,10 @@ function filter_advanced_search_fields(type,sorted_recipies_list,recipies){
     case 'appliance':
       advanced_research=document.getElementById('search_bar_appliance').value;
       list_of_the_field=[];
-      for(recipe of sorted_recipies_list){
+      for(const recipe of sorted_recipies_list){
         const myAppliance=recipe.appliance.toLowerCase();
         alreadyIn=0;
-        for(element of list_of_the_field){
+        for(const element of list_of_the_field){
           if(element.name==myAppliance){
             alreadyIn++;
           }
@@ -146,11 +146,11 @@ function filter_advanced_search_fields(type,sorted_recipies_list,recipies){
       break
     case 'ustensils':
       list_of_the_field=[];
-      for(recipe of sorted_recipies_list){
-        for(nb_of_ustensils in recipe.ustensils){
+      for(const recipe of sorted_recipies_list){
+        for(const nb_of_ustensils in recipe.ustensils){
           const myUstensil=recipe.ustensils[nb_of_ustensils].toLowerCase();
           let alreadyIn=0;
-          for(element of list_of_the_field){
+          for(const element of list_of_the_field){
             if(element.name==myUstensil){
               alreadyIn++;
             }
@@ -173,11 +173,11 @@ function filter_advanced_search_fields_from_advanced_search_bar(type,list_of_tag
   }
   let i=0;
   let list_of_the_field=[];
-  for (tag of list_of_tags){
+  for (const tag of list_of_tags){
     if(tag.type==type && research.length<=tag.name.length){
-      for(i=0;i+research.length<=tag.name.length;i++){
+      for(const i=0;i+research.length<=tag.name.length;i++){
         let test=0;
-        for (j=0;j<research.length;j++){
+        for (const j=0;j<research.length;j++){
           if(tag.name[i+j]==research[j]){
             test++;
           }
@@ -212,7 +212,7 @@ function display_recipies(sorted_recipies_list,recipies){
     }
     recipiesContainer.innerHTML=recipeCardHtml;
   }
-  for (type of ['ingredients','appliance','ustensils']){
+  for (const type of ['ingredients','appliance','ustensils']){
     filter_advanced_search_fields(type,sorted_recipies_list,recipies);
   }
   document.querySelectorAll('.a_recipe').forEach(element => {
@@ -229,10 +229,10 @@ function display_clickable_advanced_field(type,list_of_the_field,recipies){
     list_of_displayed_tags.push(aTag);
   });
   document.getElementById(type+'_list').innerHTML="";
-  for(let i=0;i<list_of_the_field.length;i++){;
+  for(const  i=0;i<list_of_the_field.length;i++){;
     let tag=list_of_the_field[i];
     let dont=0;
-    for(aTag of list_of_displayed_tags){
+    for(const aTag of list_of_displayed_tags){
       if(aTag.type==tag.type && aTag.name==tag.name){
         dont++;
       }
@@ -258,7 +258,7 @@ function display_tags(newTag,recipies){
   list_of_displayed_tags.push(newTag);
   list_of_displayed_tags=[... new Set(list_of_displayed_tags)];
   tagCardHtml='';
-  for (let i=0; i<list_of_displayed_tags.length;i++){
+  for (const i=0; i<list_of_displayed_tags.length;i++){
     tagCardHtml+=`<span class="aTag ${list_of_displayed_tags[i].type}">${capitalizeFirstLetter(list_of_displayed_tags[i].name)}<i class="fas fa-times close_tag"></i></span>`
   }
   document.getElementById('tags').innerHTML=tagCardHtml;
@@ -290,14 +290,14 @@ function open_modal(e,recipies){
   document.getElementById('chosen_recipe').classList.remove("hidden");
   if(recipe_id!=0){
     let recipe={};
-    for(element of recipies){
+    for(const element of recipies){
       if (element.id==recipe_id){
         recipe=element;
         break
       }
     }
     let list_of_ingredients=[];
-    for (item of recipe.ingredients){
+    for (const item of recipe.ingredients){
       list_of_ingredients.push('</br>'+item.ingredient);
     }
     document.querySelector('.recipe_content').innerHTML=`
